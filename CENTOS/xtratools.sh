83b3f5adcabad6ef4eec7264b9d80399f4f54b89
SivaRamCode <67477195+SivaRamCode@users.noreply.github.com>
2021-08-26 18:34:45 +0800

    Create xtratools.sh

---
+++ CENTOS/xtratools.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Created by https://github.com/SivaRamCode
+# Modified by Jeeva
+
+#Install Basic Monitoring Tools
+yum -y install epel-release 
+sleep 2
+yum -y install nload nmon nethogs htop
+
+sleep2
+cd
+
+#Requirement for curl
+if [ ! -e /usr/bin/curl ]; then
+   yum -y update && yum -y upgrade
+   yum -y install curl 
+   
+fi
+#Requirement for wget
+if [ ! -e /usr/bin/wget ]; then
+   yum -y install wget
+fi
+
+#clearing history
+history -c
+
+# info
+clear
+echo " "
+echo "INSTALLATION COMPLETE!"
+sleep 10
+
+yum reboot
