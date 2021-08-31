344b286160e50d5f7cf5f60b5c63b52bcf148e21
SivaRamCode <67477195+SivaRamCode@users.noreply.github.com>
2021-08-31 08:54:14 +0800

    Update ubuntuBasic.sh

--- data/ubuntuBasic.sh
+++ data/ubuntuBasic.sh
@@ -51,7 +51,7 @@ http_access allow localhost
 http_access deny all
 http_port 8080
 http_port 3128
-coredump_dir /var/spool/squid3
+coredump_dir /var/spool/squid
 refresh_pattern ^ftp: 1440 20% 10080
 refresh_pattern ^gopher: 1440 0% 1440
 refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
